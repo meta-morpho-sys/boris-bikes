fe5932bfb36e7ae8c5438cf925c0a96f4334fed2
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-07-05 16:25:07 +0100

    Initial commit - class creation

---
+++ spec/docking_station_spec.rb
@@ -0,0 +1,12 @@
+# In spec/docking_station_spec.rb
+require 'docking_station'
+
+describe DockingStation do
+
+  it 'responds to release_bike' do # The instance is expected to respond to message release_bike.
+    expect(subject).to respond_to :release_bike
+  end
+
+  it { is_expected.to respond_to :release_bike}
+end
+
