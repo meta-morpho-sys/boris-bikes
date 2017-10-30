9bd1227f8c0a74327307005905cf62e114adf378
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-10-31 00:56:42 +0200

    Van retrieving the bike

--- lib/van.rb
+++ lib/van.rb
@@ -1,8 +1,14 @@
+require_relative 'bike'
+require_relative 'docking_station'
+
 class Van
+  VAN_CAPACITY = 5
+
   def initialize
     @transported_bikes = []
   end
-  def retrieve_bike(bike)
+  def retrieve_bike(docking_station)
+    bike = docking_station.repair_bike
     @transported_bikes << bike
   end
 end
--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -4,10 +4,14 @@ describe Van do
   let(:docking_station) { double :docking_station }
   let(:bike) { double :bike }
 
-  context 'when retrieves a broken bike'do
-    it 'retrieves the bike from the docking station' do
-      expect(subject.retrieve_bike(bike)).to eq [bike]
-    end
+  it 'has capacity'do
+    expect(described_class::VAN_CAPACITY).to eq 5
   end
 
+  # context 'when retrieves a broken bike'do
+  #   it 'retrieves the bike from the docking station' do
+  #     allow(docking_station).to receive(:send_to_repair).and_return(bike)
+  #     expect(subject.retrieve_bike(docking_station)).to eq [bike]
+  #   end
+  # end
 end
