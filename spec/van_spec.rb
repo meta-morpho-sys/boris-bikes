f5f5e2094195a9289b2c05a750b0fc769dbcd239
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-10-31 01:03:19 +0200

    added test for retrieve bike

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -8,10 +8,10 @@ describe Van do
     expect(described_class::VAN_CAPACITY).to eq 5
   end
 
-  # context 'when retrieves a broken bike'do
-  #   it 'retrieves the bike from the docking station' do
-  #     allow(docking_station).to receive(:send_to_repair).and_return(bike)
-  #     expect(subject.retrieve_bike(docking_station)).to eq [bike]
-  #   end
-  # end
+  context 'when retrieves a broken bike'do
+    it 'retrieves the bike from the docking station' do
+      allow(docking_station).to receive(:send_to_repair).and_return(bike)
+      expect(subject.retrieve_bike(docking_station)).to eq [bike]
+    end
+  end
 end
