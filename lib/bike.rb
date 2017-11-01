f10d4ce02ebb55857a5db365b1ca2447f2c0b51c
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-11-01 20:05:06 +0200

    tests for report_fixed method of bike

--- lib/bike.rb
+++ lib/bike.rb
@@ -7,12 +7,12 @@ class Bike
     !broken?
   end
 
-  def report_broken
-    @broken = true
-  end
-
   def broken?
     @broken
+  end
+
+  def report_broken
+    @broken = true
   end
 
   def report_fixed
--- spec/bike_spec.rb
+++ spec/bike_spec.rb
@@ -4,17 +4,30 @@ describe Bike do
   it { is_expected.to respond_to :working? }
   it { is_expected.to respond_to :broken? }
 
-  it 'is #working' do
-    expect(subject).to be_working
+  it 'is working when initialized' do
+    expect(subject).not_to be_broken
   end
 
-  it 'can be reported broken' do
+  it 'is #working?' do
+    expect(subject.working?).to eq true
+  end
+
+  it 'is #broken?' do
+    expect(subject.broken?).to eq false
+  end
+
+  it 'can be #reported_broken' do
     subject.report_broken
     expect(subject).to be_broken
   end
 
-  it 'should not work if broken' do
+  it 'does not work if broken' do
     subject.report_broken
     expect(subject).not_to be_working
+  end
+
+  it 'can be #reported_fixed' do
+    subject.report_fixed
+    expect(subject).not_to be_broken
   end
 end
