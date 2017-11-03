eb11c75f96d0da855e9dd309cd53221664666032
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-11-03 18:30:04 +0200

    changed parameters for an exmpale

--- spec/garagee_spec.rb
+++ spec/garagee_spec.rb
@@ -4,12 +4,12 @@ describe Garage do
   let(:van) { double :van }
   let(:bike) { double :bike }
   before(:each) do
-    expect(van).to receive(:deliver_bike).with(1, subject).and_return(bike)
+    expect(van).to receive(:deliver_bike).with(2, subject).and_return(bike)
   end
 
   describe 'fixes broken bikes' do
     it 'accepts bikes to fix' do
-      van.deliver_bike(1, subject)
+      van.deliver_bike(2, subject)
       expect(subject.accept_to_fix(bike)).to eq [bike]
     end
   end
