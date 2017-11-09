e832a9b4e087c575cd531c1bbf2a4e158b2ffac2
Yuliya Nedyalkova <yuliya.nedyalkova777@gmail.com>
2017-11-09 13:48:04 +0000

    refactored the spec to accept a feature of retrieving a bike from garage

--- spec/van_spec.rb
+++ spec/van_spec.rb
@@ -1,7 +1,7 @@
 require 'van'
 
-def retrieve_bike
-  subject.retrieve_bike(docking_station)
+def retrieve_bike(source)
+  subject.retrieve_bike(source)
 end
 
 describe Van do
@@ -15,16 +15,16 @@ describe Van do
   end
 
   it 'has capacity' do
-    described_class::VAN_CAPACITY.times { retrieve_bike }
-    expect { retrieve_bike }.to raise_error 'Van is full.'
+    described_class::VAN_CAPACITY.times { retrieve_bike(docking_station) }
+    expect { retrieve_bike(docking_station) }.to raise_error 'Van is full.'
   end
 
   context 'when managing a broken bike' do
     it 'retrieves the bike from the docking station' do
-      expect(retrieve_bike).to eq [bike]
+      expect(retrieve_bike(docking_station)).to eq [bike]
     end
     it 'delivers the bike to the garage' do
-      3.times { retrieve_bike }
+      3.times { retrieve_bike(docking_station) }
       expect do
         subject.deliver_bike(2, garage)
       end.to change { subject.transported_bikes.size }.by(-2)
